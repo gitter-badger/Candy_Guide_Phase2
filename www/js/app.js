d56dca54da01dce97608c70a298cf0271ef224ea
YusukeShimizu <stm1051212@gmail.com>
2016-07-24 21:51:46 +0900

    UI creating

--- www/js/app.js
+++ www/js/app.js
@@ -40,12 +40,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.friends', {
+    url: '/friends',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-friends': {
+        templateUrl: 'templates/tab-friends.html',
+        controller: 'friendsCtrl'
       }
     }
   })
@@ -55,16 +55,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       views: {
         'tab-chats': {
           templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+          controller: 'chatsCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+
+  .state('tab.chat-detail', {
+      url: '/chats/candy',
       views: {
         'tab-chats': {
           templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+          controller: 'chatDetailCtrl'
         }
       }
     })
@@ -80,6 +81,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/friends');
 
 });
