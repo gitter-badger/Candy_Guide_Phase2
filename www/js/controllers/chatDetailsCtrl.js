32d0b02317bc56eb0205fea5f44fe8d976513a10
MiyajimaDaichi <let.it.be7daichi@gmail.com>
2016-09-04 11:13:39 +0900

    add test version of cordova

--- www/js/controllers/chatDetailsCtrl.js
+++ www/js/controllers/chatDetailsCtrl.js
@@ -1,3 +1,37 @@
-candyCtrl.controller('chatDetailsCtrl', function($scope) {
-    
+candyCtrl.controller('chatDetailsCtrl', function($scope, $ionicPlatform, $cordovaGeolocation, $cordovaGoogleMap, $cordovaDeviceOrientation) {
+    $ionicPlatform.ready(function() {
+        console.log(window);
+        console.log($cordovaGeolocation);
+        
+        //$cordovaGoogleMap.getMap()
+        
+        $cordovaGoogleMap.getMap({zoom: 12}).then(function(result) {
+            console.log(result);
+        }, function(err) {
+            // An error occurred
+            console.log(err);
+        });
+        
+        $cordovaDeviceOrientation.getCurrentHeading().then(function(result) {
+           var magneticHeading = result.magneticHeading;
+           $scope.test = result.magneticHeading;
+           console.log("pass");
+           var trueHeading = result.trueHeading;
+           var accuracy = result.headingAccuracy;
+           var timeStamp = result.timestamp;
+           console.log(result);
+        }, function(err) {
+            // An error occurred
+            console.log(err);
+            $scope.test = err;
+        });
+        
+        var posOptions = {timeout: 10000, enableHighAccuracy: false};
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            console.log(position.coords);
+        }, function(err) {
+            // error
+            console.log("error");
+        });
+    });
 });
